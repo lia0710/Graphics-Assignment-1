2529e6c3dcfc7dfd9cc336c604c787b8d807e46f
Lia Fisher <willfisher1007@hotmail.ca>
2023-10-13 13:44:46 -0400

    Import models
    Imported the sphere and cube

--- Assignment 1/Game1.cs
+++ Assignment 1/Game1.cs
@@ -6,28 +6,37 @@ namespace Assignment_1
 {
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager device;
+        Model spheremodel;
+        Model cubemodel;
+        private SpriteBatch spriteBatch;
+
+        private Matrix world = Matrix.Identity;
+        private Matrix view = Matrix.Identity;
+        private Matrix projection = Matrix.Identity;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            device = new GraphicsDeviceManager(this);
+            device.PreferredBackBufferWidth = 700;
+            device.PreferredBackBufferHeight = 700;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+            view = Matrix.CreateLookAt(new Vector3(0, 40, 10), new Vector3(0, 0, 0), Vector3.Up);
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), device.GraphicsDevice.Viewport.AspectRatio, 0.1f, 100f);
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            spheremodel = Content.Load<Model>("Sphere");
+            cubemodel = Content.Load<Model>("Assignment1Cube");
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,9 +51,12 @@ namespace Assignment_1
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            foreach (var mesh in spheremodel.Meshes)
+            {
+                mesh.Draw();
+            }
 
             base.Draw(gameTime);
         }
